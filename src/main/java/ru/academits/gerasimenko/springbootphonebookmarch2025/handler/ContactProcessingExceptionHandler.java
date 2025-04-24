5bfe5c87b4ccf09193adebbf59e02aa328d573ab
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-04-24 17:59:45 +0700

    add logging in services and a scheduled task to deletion random contact one time in 10 seconds

--- src/main/java/ru/academits/gerasimenko/springbootphonebookmarch2025/handler/ContactProcessingExceptionHandler.java
+++ src/main/java/ru/academits/gerasimenko/springbootphonebookmarch2025/handler/ContactProcessingExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.academits.gerasimenko.springbootphonebookmarch2025.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +10,13 @@ import ru.academits.gerasimenko.springbootphonebookmarch2025.data.Response;
 import ru.academits.gerasimenko.springbootphonebookmarch2025.exception.ContactProcessingException;
 
 @ControllerAdvice
+@Slf4j
 public class ContactProcessingExceptionHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public Response catchContactProcessingException(ContactProcessingException e) {
+        log.warn("Exception caught: {}", e.getMessage());
         return new Response(false, e.getMessage());
     }
 }
--- src/main/java/ru/academits/gerasimenko/springbootphonebookmarch2025/service/PhoneBookServiceImpl.java
+++ src/main/java/ru/academits/gerasimenko/springbootphonebookmarch2025/service/PhoneBookServiceImpl.java
@@ -1,15 +1,22 @@
 package ru.academits.gerasimenko.springbootphonebookmarch2025.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.academits.gerasimenko.springbootphonebookmarch2025.data.Contact;
 import ru.academits.gerasimenko.springbootphonebookmarch2025.data.ContactProcessor;
 import ru.academits.gerasimenko.springbootphonebookmarch2025.repository.PhoneBookRepository;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
+@EnableScheduling
+@Slf4j
 public class PhoneBookServiceImpl implements PhoneBookService {
     private final PhoneBookRepository phoneBookRepository;
+    private final Random randomIndexGenerator = new Random();
 
     public PhoneBookServiceImpl(PhoneBookRepository phoneBookRepository) {
         this.phoneBookRepository = phoneBookRepository;
@@ -17,36 +24,71 @@ public class PhoneBookServiceImpl implements PhoneBookService {
 
     @Override
     public List<Contact> getContacts(String term) {
+        log.info("A contact list containing the term \"{}\" was returned.", term);
         return phoneBookRepository.getContacts(term);
     }
 
     @Override
     public void addContact(Contact contact) {
         Contact newContact = ContactProcessor.processContact(contact);
+        log.info("Trying to add new contact");
 
         if (ContactProcessor.isCorrectContact(newContact)
                 & ContactProcessor.isNotExistingContact(phoneBookRepository, newContact)
         ) {
             phoneBookRepository.addContact(newContact);
+            log.info("New contact added successfully");
+        } else {
+            log.warn("Failed to create contact");
         }
     }
 
     @Override
     public void updateContact(Contact contact) {
         Contact newContact = ContactProcessor.processContact(contact);
+        log.info("Trying to update contact with id = {}", newContact.getId());
 
         if (ContactProcessor.isCorrectContact(newContact)
                 & ContactProcessor.isExistingContactById(phoneBookRepository, newContact.getId())
                 & ContactProcessor.isNotExistingContactOrSame(phoneBookRepository, newContact)
         ) {
             phoneBookRepository.updateContact(contact);
+
+            log.info("Contact with id = {} updated successfully", newContact.getId());
+        } else {
+            log.warn("Failed to update contact with id = {}", newContact.getId());
         }
     }
 
     @Override
     public void deleteContact(int id) {
+        log.info("Trying to delete contact with id = {}", id);
+
         if (ContactProcessor.isExistingContactById(phoneBookRepository, id)) {
             phoneBookRepository.deleteContact(id);
+
+            log.info("Contact with id = {} deleted successfully", id);
+        } else {
+            log.warn("Failed to delete contact with id = {}", id);
         }
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void deleteRandomContact() {
+        log.info("Launched scheduled task that deletes random contacts every 10 seconds");
+
+        List<Contact> contacts = phoneBookRepository.getContacts("");
+        int contactsAmount = contacts.size();
+
+        if (contactsAmount == 0) {
+            log.warn("Scheduled task not completed: contacts missing");
+            return;
+        }
+
+        Contact deletedContact = contacts.get(randomIndexGenerator.nextInt(contactsAmount));
+        log.info("Random contact id for deletion defined: id = {}", deletedContact.getId());
+
+        deleteContact(deletedContact.getId());
+        log.info("Scheduled task completed successfully");
     }
 }
